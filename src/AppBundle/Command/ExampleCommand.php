44b549406c4129491eefc127d6b126d1e79f8f64
steve <sreilly826@gmail.com>
2017-07-17 16:32:59 -0400

    Implement as Command

--- src/AppBundle/Command/ExampleCommand.php
+++ src/AppBundle/Command/ExampleCommand.php
@@ -2,19 +2,30 @@
 
 namespace AppBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-class ExampleCommand extends Command {
+use AppBundle\Utility\ZendeskApi;
+
+class ExampleCommand extends ContainerAwareCommand {
   protected function configure() {
     $this->setName('app:example');
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $output->writeln('Is this working?');
+
+    $container = $this->getContainer();
+    $zendesk = $container->get('api.zendesk');
+    //$zendesk = $this->getApplication()->getKernel()->getContainer()->get('api.zendesk');
+    $viewID = 56695823;
+
+    $ticketData = $zendesk->zendeskApiCall('GET', "/api/v2/views/".$viewID."/execute.json");
+    $ticketDataRows = $ticketData["rows"][0];
+
+    $output->writeln($ticketDataRows['ticket_id']);
   }
 
 }
